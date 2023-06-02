b77bf2e2d8c1dc41d9937e27f949c41b6cab6f3d
Niels Otterspoor <niels.otterspoor@gmail.com>
2023-06-02 14:08:25 +0200

    button story

---
+++ components/button.story.vue
@@ -0,0 +1,17 @@
+<!-- button.story.vue -->
+<template>
+    <Story>
+      <Variant title="Default">
+        <Button>Default</Button>
+      </Variant>
+      <Variant title="Small">
+        <Button isSmall>Small</Button>
+      </Variant>
+      <Variant title="Large">
+        <Button isLarge>Large</Button>
+      </Variant>
+    </Story>
+</template>
+<script setup lang="ts">
+import Button from "~/components/button.vue";
+</script>
---
+++ components/button.vue
@@ -0,0 +1,38 @@
+<!-- button.vue -->
+<template>
+    <div class="button"
+    :class="[isSmall ? 'button--small' : '', isLarge ? 'button--large' : '']"
+    >{{text}}</div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    text: {
+        type: String,
+        default: "Button",
+    },
+  isSmall: {
+        type: Boolean,
+        default: false,
+    },
+  isLarge: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<style scoped lang="css">
+.button {
+    background-color: #000;
+    color: #fff;
+  padding: 10px 20px;
+}
+
+.button--small {
+  padding: 5px 10px;
+}
+
+.button--large {
+  padding: 20px 40px;
+}
+</style>
